85e19567479609c85c0ae70894f1d5e6a30cdf0c
darkflade <dodgesrt7775@gmail.com>
2025-09-17 11:45:54 -0700

    Upld

---
+++ src/Controller.php
@@ -0,0 +1,8 @@
+<?php
+namespace Darkflade\Tic_tac_toe\Controller;
+
+use Darkflade\Tic_tac_toe\View;
+
+function startGame() {
+    View\showStartScreen();
+}
---
+++ src/View.php
@@ -0,0 +1,6 @@
+<?php
+namespace Darkflade\Tic_tac_toe\View;
+
+function showStartScreen() {
+    echo "Добро пожаловать в игру Tic-Tac-Toe!\n";
+}
